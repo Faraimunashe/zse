77c274b03e58bdd4003308b71b4c6ca1f9b1d202
Faraimunashe <faraimunashe.m11@gmail.com>
2022-11-30 00:05:37 +0200

    90775334

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -9,6 +9,6 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.brokers');
+        return redirect()->back()->route('admin-users');
     }
 }
